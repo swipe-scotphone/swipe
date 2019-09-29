cccf3a29dab695f8e24e65ec91abdfba8d7938cf
Martin Williamson <mw@toliman.co.uk>
2019-09-29 17:26:15 +0100

    Updated Site Name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'SWIPE',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Eenvironmentally friendly, premium quality, super concentrated cleaning products - direct to your home.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
